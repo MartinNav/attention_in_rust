dd7e117f8dee626b450a13c3fb1d8f865896ffd7
MartinNav <martinnavratilu@gmail.com>
2024-12-13 13:43:36 +0100

    I may have just found bug in ndarray

--- src/attention/flash.rs
+++ src/attention/flash.rs
@@ -1,7 +1,12 @@
 use std::usize;
 
-use ndarray::Array3;
-
+use ndarray::{Array3,s};
+fn min(a:usize,b:usize)->usize{
+    if a>b {
+        return b;
+    }
+    a
+}
 pub fn flash_attention(q:&Array3<f32>, k: &Array3<f32>, v:&Array3<f32>,block_size:usize)->Option<Array3<f32>> {
     let [batch_size, seq_len, head_dim]=q.shape() else{
         return None;
@@ -9,7 +14,13 @@ pub fn flash_attention(q:&Array3<f32>, k: &Array3<f32>, v:&Array3<f32>,block_siz
     let num_blocks = (seq_len+block_size-1);
     let output = Array3::zeros((*batch_size,*seq_len,*head_dim));
     for i in  0..num_blocks {
-        
+       for j in 0..num_blocks {
+           let q_block = q.slice(s![..,(i*block_size)..(min((i+1)*block_size,*seq_len)),..]);
+           let k_block = k.slice(s![..,(j*block_size)..(min((j+1)*block_size,*seq_len)),..]);
+           let v_block = v.slice(s![..,(j*block_size)..(min((j+1)*block_size,*seq_len)),..]);
+
+           let s = q_block.dot(k_block.t())/(*head_dim as f32).sqrt();
+       } 
     }
 
     Some(output)
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+//#![recursion_limit = "8192"]
 mod attention;
 fn main() {
     println!("Hello, world!");
